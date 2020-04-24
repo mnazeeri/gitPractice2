0309e86b563536d9f59622c090700f77f59c9079
mnazeeri <64224064+mnazeeri@users.noreply.github.com>
2020-04-24 15:29:29 -0500

    Update Ramadan.java

--- src/day_2/Ramadan.java
+++ src/day_2/Ramadan.java
@@ -4,7 +4,8 @@ public class Ramadan {
 
     public static void main(String[] args) {
 
-
+// this note is added from gitHub: Pleae correct the followings
+        
         System.out.println("Today is the first day of Ramadan");
     }
 }
